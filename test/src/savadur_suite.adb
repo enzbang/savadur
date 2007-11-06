57ded284ce8019dcbf2a534eca7f619749e39d2f
Olivier Ramonat <olivier@ramonat.fr>
2007-11-06 23:37:44 +0100

    Add missings copyright notice

--- test/src/savadur_suite.adb
+++ test/src/savadur_suite.adb
@@ -1,8 +1,31 @@
-with AUnit; use AUnit.Test_Suites;
+------------------------------------------------------------------------------
+--                                Savadur                                   --
+--                                                                          --
+--                           Copyright (C) 2007                             --
+--                            Olivier Ramonat                               --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.       --
+------------------------------------------------------------------------------
+
+with AUnit;
 
 with Config_Parse;
 
 package body Savadur_Suite is
+
+   use AUnit.Test_Suites;
 
    Result : aliased Test_Suite;
    Test_1 : aliased Config_Parse.Test_Case;
