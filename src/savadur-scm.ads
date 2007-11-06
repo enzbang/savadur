4729c0cc04a451988f3ffd6dfbe0b32609076323
Olivier Ramonat <olivier@ramonat.fr>
2007-11-06 22:13:00 +0100

    SCM.Action changed from Vector to Map

--- src/savadur-scm.ads
+++ src/savadur-scm.ads
@@ -15,7 +15,7 @@ package Savadur.SCM is
    Null_Uid : U_Id := U_Id (Null_Unbounded_String);
 
    type SCM is record
-      Actions : Savadur.Action.Vectors.Vector;
+      Actions : Savadur.Action.Maps.Map;
    end record;
 
    function Hash (Key : Id) return Containers.Hash_Type;
