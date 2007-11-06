47cf68a5f0127d0f3b5cc3c4d7c3d236d28a82ea
Olivier Ramonat <olivier@ramonat.fr>
2007-11-06 22:15:16 +0100

    Minor changes

--- src/savadur-client.adb
+++ src/savadur-client.adb
@@ -2,13 +2,17 @@
 with Ada.Text_IO;
 with Ada.Strings.Unbounded;
 
-with Savadur.Config;
+with Savadur.Config.Project;
+with Savadur.Config.SCM;
 with Savadur.Action;
 with Savadur.Scenario;
+with Savadur.SCM;
 
 procedure Savadur.Client is
-   Project : Savadur.Config.Project_Config :=
-               Savadur.Config.Parse ("test/savadurrc");
+   Project : Savadur.Config.Project.Project_Config :=
+               Savadur.Config.Project.Parse ("test/savadurrc");
+
+   SCM_Map : Savadur.SCM.Maps.Map := Savadur.Config.SCM.Parse ("config/scm");
 
    use Ada.Text_IO;
    use Ada.Strings.Unbounded;
@@ -26,4 +30,11 @@ begin
    New_Line;
    Put_Line (Savadur.Scenario.Image (Project.Scenari));
    New_Line;
+   New_Line;
+   New_Line;
+   Put_Line ("SCM Found");
+   New_Line;
+   Put_Line (Savadur.SCM.Image (SCM_Map));
+
+
 end Savadur.Client;
