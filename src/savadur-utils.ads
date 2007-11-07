d681aab7311c47ee63967ec45b075af0f8970566
Olivier Ramonat <olivier@ramonat.fr>
2007-11-07 12:24:53 +0100

    Add Utils package

---
+++ src/savadur-utils.ads
@@ -0,0 +1,35 @@
+------------------------------------------------------------------------------
+--                                Savadur                                   --
+--                                                                          --
+--                           Copyright (C) 2007                             --
+--                            Olivier Ramonat                               --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.       --
+------------------------------------------------------------------------------
+
+with Ada.Strings.Unbounded;
+
+package Savadur.Utils is
+
+   use Ada;
+   use Ada.Strings.Unbounded;
+
+   function "+" (Source : in String) return Unbounded_String
+                 renames To_Unbounded_String;
+
+   function "-" (Source : in Unbounded_String) return String
+                 renames To_String;
+
+end Savadur.Utils;
