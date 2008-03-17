82b7c145e61d76bb9d7da8005cf4d889058b51b0
Pascal Obry <pascal@obry.net>
2008-03-17 20:23:37 +0100

    Minor reformatting

--- db/data/create_database.sh
+++ db/data/create_database.sh
@@ -11,40 +11,39 @@ rm -f ${DATABASE_NAME}
 
 cat <<EOF | ${SQLITE} ${DATABASE_NAME}
 create table sessions (
-"client" varchar(50) not null,
-login_date date default current_timestamp,
-logout_date date default current_timestamp
+   "client" varchar(50) not null,
+   login_date date default current_timestamp,
+   logout_date date default current_timestamp
 );
 
 create table job_id (
-id integer primary key autoincrement
+   id integer primary key autoincrement
 );
 
 create table lastbuilt (
-"client" varchar(50) not null,
-"project"  varchar(50) not null,
-"scenario" varchar(50) not null,
-"status" boolean,
-"job_id" integer,
-"date" date default current_timestamp
+   "client" varchar(50) not null,
+   "project" varchar(50) not null,
+   "scenario" varchar(50) not null,
+   "status" boolean,
+   "job_id" integer,
+   "date" date default current_timestamp
 );
 
 create table logs (
-"client" varchar(50) not null,
-"project"  varchar(50) not null,
-"scenario" varchar(50) not null,
-"action" varchar(50) not null,
-"log" longtext,
-"filename" varchar(255),
-"status" boolean,
-"job_id" integer,
-"date" date default current_timestamp
+   "client" varchar(50) not null,
+   "project" varchar(50) not null,
+   "scenario" varchar(50) not null,
+   "action" varchar(50) not null,
+   "log" longtext,
+   "filename" varchar(255),
+   "status" boolean,
+   "job_id" integer,
+   "date" date default current_timestamp
 );
 
 create table notify (
-"project" varchar(50) not null,
-"email" varchar(255) unique,
-"xmpp" varchar(255) unique
+   "project" varchar(50) not null,
+   "email" varchar(255) unique,
+   "xmpp" varchar(255) unique
 );
-
 EOF
